f434f48d57a42d3f5fb105075d72cb44dd532f07
mosaicjide <mosaicjide@gmail.com>
2023-02-17 20:04:28 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,23 +1,41 @@
 /*
- * File: 2-print_alphabet.c
+ * File: 1-last_digit.c
  * Auth: Moses Babajide
  */
 
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char letter;
+	int n;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-	putchar('\n');
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
+	}
 
 	return (0);
+
 }
