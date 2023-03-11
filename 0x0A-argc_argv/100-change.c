3de6d7faee31ed30a5b56e8a6b25b0fdcb1f12c4
mosaicjide <mosaicjide@gmail.com>
2023-03-10 22:53:22 -0800

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,51 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - prints the minimum number of coins to make change for an
- * amount of money
- * @argc: the number of arguments passed to the function
- * @argv: an aray of the commanf line arguments passed to the function
- * Return: 0 on success
+ *main - funtion
+ *@argc: legenth of argv
+ *@argv: number of argummet
+ *Return: Always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int cent_vals[] = {25, 10, 5, 2, 1};
-	int cents, number_of_cents, balance, i;
+	/*Declaring variables*/
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+
+	position = total = change = aux = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
+
+	total = atoi(argv[1]); /*Convert str to int*/
+
+	if (total <= 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	number_of_cents = 0;
-	balance = cents;
-	for (i = 0; i < 5; ++i)
+	/*Declaring WHILE*/
+
+	while (coins[position] != '\0')
+
 	{
-		number_of_cents += balance / cent_vals[i];
-		if ((balance % cent_vals[i]) == 0)
-			break;
-		balance %= cent_vals[i];
+		if (total >= coins[position])
+		{
+			aux = (total / coins[position]);
+			change += aux;
+			total -= coins[position] * aux;
+		}
+
+		position++;
+
 	}
-	printf("%d\n", number_of_cents);
+
+	printf("%d\n", change);
 	return (0);
 }
